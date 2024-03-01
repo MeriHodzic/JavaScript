da050d2288f4869be7546b3dce065e7408ee72c5
merihodzic <merihodzic123@gmail.com>
2024-03-01 20:09:01 +0100

    35.cas

---
+++ 35.cas/index.js
@@ -0,0 +1,26 @@
+//async await
+ //try catch,http
+
+// const asyncFn = async () => {
+  
+//     const rand = math.random();
+//     if (rand > 0.5) {
+//       console.log("we are in resolve");
+//       return "resolve";
+//     } else {
+//       console.log("we are in reject");
+//       throw new Error("This is Error text");
+//     }
+//   }
+
+// console.log(
+//   asyncFn().then((result) => {
+//     console.log(result);
+//   })
+//   .then((result)=>
+// ));
+
+
+//internet,iso model,tcp and udp!!
+//http
+//http methods
---
+++ 36.cas/index.js
@@ -0,0 +1,5 @@
+//fetching data & query params
+
+//fechajte sa ovog apija    sa get methodom podatke i zatim ih consologujte.Koristi te promise i fetch api
+
+fetch("");
